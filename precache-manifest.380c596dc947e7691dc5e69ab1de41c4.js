f4073c593429b7fef11859abbd05cfb7f9d37e8a
lihqi <455711093@qq.com>
2020-07-27 13:32:43 +0000

    Deploying to gh-pages from master 71bcd6c3891a8d3c6e4708d46f321a75d5f6de34

---
+++ precache-manifest.380c596dc947e7691dc5e69ab1de41c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1dde6c02e4b942122ea22dbeb4523e",
+    "url": "/remove-antd-menu-animation/index.html"
+  },
+  {
+    "revision": "aa5bcfa626f577a5c6ba",
+    "url": "/remove-antd-menu-animation/static/css/2.cf37ca46.chunk.css"
+  },
+  {
+    "revision": "30934b680ac1283ef038",
+    "url": "/remove-antd-menu-animation/static/css/main.8ea81e53.chunk.css"
+  },
+  {
+    "revision": "aa5bcfa626f577a5c6ba",
+    "url": "/remove-antd-menu-animation/static/js/2.c2313791.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/remove-antd-menu-animation/static/js/2.c2313791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30934b680ac1283ef038",
+    "url": "/remove-antd-menu-animation/static/js/main.4bb06bed.chunk.js"
+  },
+  {
+    "revision": "2f810f077a991f0a2836",
+    "url": "/remove-antd-menu-animation/static/js/runtime-main.2e5dc3f0.js"
+  }
+]);
